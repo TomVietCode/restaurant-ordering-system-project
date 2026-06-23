a20102ada505b9566e0f4463221720eddc066d07
NVD06 <concoko35@gmail.com>
2026-06-23 11:57:08 +0700

    creat layout and use mock

--- code/backend/src/modules/categories/categories.module.ts
+++ code/backend/src/modules/categories/categories.module.ts
@@ -4,9 +4,12 @@ import { Category } from './entities/category.entity.js';
 import { CategoryRepository } from './repositories/category.repository.js';
 import { CategoriesService, CATEGORY_REPOSITORY_TOKEN } from './categories.service.js';
 import { CategoriesController } from './categories.controller.js';
+import { ItemsModule } from '@modules/items/item.module.js';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [TypeOrmModule.forFeature([Category]), 
+  forwardRef(() => ItemsModule)
+],
   controllers: [CategoriesController],
   providers: [
     // Bind the token to the concrete CategoryRepository
